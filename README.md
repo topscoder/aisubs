867cd84515e7f3f0f8b6eb94f7ad94b4b14b2b34
topscoder <86197446+topscoder@users.noreply.github.com>
2023-05-26 18:58:10 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# aisubs
+# AI Subdomain Generator - aisubs
+
+This is a script that generates new subdomains based on a given subdomain using the power of the OpenAI GPT-3.5 Turbo model. It takes input from either a text file or command line and generates similar subdomains by interacting with the OpenAI API.
+
+## Prerequisites
+
+- Go programming language (version 1.16 or higher)
+- OpenAI API key
+
+## Installation
+
+```bash
+go install github.com/your-username/aisubs@latest
+```
+
+## Usage
+
+1. The script can be used in two ways:
+
+    ```bash
+    cat subdomains.txt | aisubs --apikey <OpenAI API Key> --amount 5
+    ```
+
+    Replace `<OpenAI API Key>` with your actual OpenAI API key.
+
+2. Providing input via command line:
+
+    ```bash
+    echo "www.domain.com" | go run main.go --apikey <OpenAI API Key> --amount 5
+    ```
+
+    Replace `<OpenAI API Key>` with your actual OpenAI API key.
+
+And it's able to use in your leet command chains. Eg.
+
+```bash
+ cat subdomains.txt | aisubs --apikey <OpenAI API Key> --amount 5 | httpx -ip -sc -cl -title -silent
+```
+
+The script reads input from either a text file or command line, where each line represents a subdomain in the format `<subdomain>.<domain>`. It generates `amount` number of new subdomains similar to the provided subdomain and prints them to the standard output.
+
+## Configuration
+
+Before running the script, make sure to obtain an API key by signing up on the [OpenAI website](https://openai.com/).
+
+## Example
+
+Let's say we have a text file called subdomains.txt with the following content:
+
+```
+www.example.com
+api.example.com
+```
+
+We can generate 3 new subdomains similar to www.example.com using the script:
+
+```bash
+cat subdomains.txt | go run main.go --apikey <OpenAI API Key> --amount 3
+```
+
+The script will output something like this:
+
+```
+web.example.com
+blog.example.com
+shop.example.com
+```
+
+## Limitations
+
+* The script relies on the OpenAI GPT-3.5 Turbo model and requires a valid OpenAI API key.
+* The effectiveness of subdomain generation depends on the quality and diversity of the training data used to train the model.
+* The script may encounter rate limits or errors when making API requests. It automatically handles rate limit errors by waiting for 20 seconds before retrying.
+* The script assumes the input subdomains are in the format `<subdomain>.<domain>`.
+
+## Contributing
+
+If you would like to contribute to this project, please feel free to fork the repository and submit a pull request.
+
+
+## License
+
+This project is licensed under the MIT License.
+
+
+## Contact
+
+If you have any questions or feedback, please feel free to contact the project maintainers.
---
+++ main.go
@@ -0,0 +1,125 @@
+package aisubs
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/sashabaranov/go-openai"
+	"golang.org/x/net/publicsuffix"
+)
+
+func generateSubdomains(subdomain, domain, apiKey string, amount string) ([]string, error) {
+	client := openai.NewClient(apiKey)
+
+	for {
+		resp, err := client.CreateChatCompletion(
+			context.Background(),
+			openai.ChatCompletionRequest{
+				Model: openai.GPT3Dot5Turbo,
+				Messages: []openai.ChatCompletionMessage{
+					{
+						Role:    openai.ChatMessageRoleSystem,
+						Content: "You are a helpful assistant that generates new subdomains based on a given subdomain.",
+					}, {
+						Role:    openai.ChatMessageRoleUser,
+						Content: "Generate " + amount + " subdomains similar to " + subdomain + ". Only reply with the bare subdomains, each on a new line without additional text.",
+					},
+				},
+			},
+		)
+
+		if err != nil {
+			if strings.Contains(err.Error(), "Rate limit exceeded") || strings.Contains(err.Error(), "That model is currently overloaded with other requests") {
+				fmt.Println("Rate limit exceeded. Sleeping for 20 seconds...")
+				time.Sleep(20 * time.Second)
+				continue
+			}
+			return nil, fmt.Errorf("error generating subdomains: %w", err)
+		}
+
+		choices := resp.Choices
+		if len(choices) == 0 {
+			return nil, fmt.Errorf("no subdomains generated")
+		}
+
+		aiGeneratedSubdomains := make([]string, 0, len(choices[0].Message.Content))
+		lines := strings.Split(choices[0].Message.Content, "\n")
+
+		for _, line := range lines {
+			if !strings.Contains(line, " ") {
+				continue
+			}
+
+			if !strings.Contains(line, ".") {
+				continue
+			}
+
+			parts := strings.Split(line, " ")
+			result := parts[1]
+
+			domainparts := strings.Split(result, ".")
+			subdomain := domainparts[0]
+
+			mainDomain, err := extractMainDomain(domain)
+			if err != nil {
+				fmt.Println("Error: ", err)
+				continue
+			}
+
+			aiGeneratedSubdomains = append(aiGeneratedSubdomains, fmt.Sprintf("%s.%s", subdomain, mainDomain))
+		}
+
+		return aiGeneratedSubdomains, nil
+	}
+}
+
+func extractMainDomain(domain string) (string, error) {
+	mainDomain, err := publicsuffix.EffectiveTLDPlusOne(domain)
+	if err != nil {
+		return "", err
+	}
+	return mainDomain, nil
+}
+
+func main() {
+	if len(os.Args) < 5 {
+		fmt.Println("Usage: cat subdomains.txt | go run main.go --apikey <OpenAI API Key> --amount 5 | httpx -ip -sc -cl -title -silent")
+		fmt.Println("Usage: echo www.domain.com | go run main.go --apikey <OpenAI API Key> --amount 5")
+		return
+	}
+
+	apiKey := os.Args[2]
+	amount := os.Args[4]
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "*") {
+			continue
+		}
+
+		parts := strings.SplitN(line, ".", 2)
+		if len(parts) != 2 {
+			fmt.Println("Invalid input format. Expected <subdomain>.<domain>.")
+			continue
+		}
+
+		subdomain, domain := parts[0], parts[1]
+
+		newSubdomains, err := generateSubdomains(subdomain, domain, apiKey, amount)
+		if err != nil {
+			fmt.Printf("Error generating subdomains: %v\n", err)
+			continue
+		}
+
+		fmt.Println(strings.Join(newSubdomains, "\n"))
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading input: %v\n", err)
+	}
+}
